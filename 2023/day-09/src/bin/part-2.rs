9662131205132661db6b6b80f01c56a35c54ea62
Thomas Matern <thomas.matern@seequent.com>
2023-12-12 09:39:37 +1300

    Day 9

---
+++ 2023/day-09/src/bin/part-2.rs
@@ -0,0 +1,60 @@
+#![allow(unused_variables,unused_imports,dead_code, unused_mut)]
+
+use nom::{IResult, Parser, character::complete::{line_ending, space1, self}, multi::separated_list1};
+
+
+fn main() {
+    let input = include_str!("./input-1.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn parse(i: &str) -> IResult<&str, Vec<Vec<isize>>> {
+    let (i, histories) = 
+        separated_list1(line_ending,
+            separated_list1(space1, complete::i128.map(|x| x as isize))
+        )
+        .parse(i)?;
+    Ok((i, histories))
+}
+
+pub fn process(i: &str) -> String {
+    let (_, histories) = parse(i).unwrap();
+    histories
+        .iter()
+        .map(|row| {
+            let mut first_column:Vec<isize> = Vec::new();
+            let mut row = row.clone();   
+            loop {
+                first_column.push(*row.first().unwrap());
+                row = row.iter().zip(row.iter().skip(1))
+                    .map(|(&a, &b)| b-a)
+                    .collect::<Vec<_>>();
+                if row.len() == 0 || row.iter().all(|&x| x == 0) {
+                    break;
+                }
+            }
+            first_column
+                .iter()
+                .rev()
+                .fold(
+                    0,
+                    |prev, value| {value - prev}
+                )          
+        })
+        .sum::<isize>().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::process;
+
+    #[test]
+    fn test_process() {
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+";
+        assert_eq!("2", process(input));
+    }
+}
--- 2023/template/src/bin/part-1.rs
+++ 2023/template/src/bin/part-1.rs
@@ -1,5 +1,8 @@
 #![allow(unused_variables,unused_imports,dead_code)]
 
+use nom::{IResult, Parser, character::complete::{alphanumeric1, line_ending}, multi::{separated_list1, count}, sequence::separated_pair};
+use nom_supreme::{tag::complete::tag, ParserExt};
+
 fn main() {
     let input = include_str!("./input-1.txt");
     let output = process(input);
